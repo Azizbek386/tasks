dca0ccd8293b0d8126837b4b53557ddaf70fdb79
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-04-14 23:25:22 +0500

    I added a polymorphic relationship.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,18 +1,30 @@
 <?php
 
+
 namespace App\Models;
+use App\Models\Image;
+
+
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Task extends Model
 {
-    protected $table = 'tasks';  
+    protected $table = 'tasks';
+
     protected $fillable = [
         'task_name',
-        'priority',  
-        'due_date',  
-        'status',    
-        'description', 
-        'image'
+        'priority',
+        'due_date',
+        'status',
+        'description',
+        'image',
     ];
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
+
